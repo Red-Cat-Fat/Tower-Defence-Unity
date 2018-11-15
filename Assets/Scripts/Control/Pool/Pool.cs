d188fc01c013fc83ca151fd6d8050ba06dd02996
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2018-11-15 14:42:14 +0300

    Create Rotator component. Convert Pool to standart class.

--- Assets/Scripts/Control/Pool/Pool.cs
+++ Assets/Scripts/Control/Pool/Pool.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pool : MonoBehaviour {
+public class Pool {
     private GameObject _prefab;
     private Transform _container;
     private List<PoolObject> _objects = new List<PoolObject>();
